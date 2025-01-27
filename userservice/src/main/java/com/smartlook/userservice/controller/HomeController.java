0757be7aa600bb9344b99ad5d486ce35b194ecf0
Deepak <deepakmoud@yahho.com>
2025-01-27 17:20:15 +0000

    user service created

---
+++ userservice/src/main/java/com/smartlook/userservice/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.smartlook.userservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String welcome(){
+        return "welcome to smartlook...";
+    }
+
+    @RequestMapping("/home")
+    public String home(){
+        return "this is home page";
+    }
+}
---
+++ userservice/src/test/java/com/smartlook/userservice/UserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartlook.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
